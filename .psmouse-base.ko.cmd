6b6577e9d84aab012ed8f9d7e019230ddace2630
xu <gwong163@163.com>
2012-11-28 20:53:32 +0800

    delete something

--- .psmouse-base.ko.cmd
+++
@@ -1,1 +0,0 @@
-cmd_/home/xu/srckern/mouse/psmouse-base.ko := ld -r -m elf_i386 -T /usr/src/linux-headers-3.2.0-3-common/scripts/module-common.lds --build-id  -o /home/xu/srckern/mouse/psmouse-base.ko /home/xu/srckern/mouse/psmouse-base.o /home/xu/srckern/mouse/psmouse-base.mod.o
